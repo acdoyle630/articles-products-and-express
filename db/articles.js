72a1665202087f8c956bba4db708011f1db0cd22
Adam <acdoyle630@gmail.com>
2017-04-28 15:45:35 -1000

    added scss

--- db/articles.js
+++ db/articles.js
@@ -15,6 +15,16 @@ const articleDataBase = () =>{
     });
   }
 
+  function getID(id){
+    console.log('get id: ' + id);
+    newId = id.split('/').join('');
+    console.log(newId);
+    return db.one(`SELECT * FROM articles WHERE articles.id = ${newId}`)
+    .catch((error) =>{
+      console.log(error);
+    });
+  }
+
   function post ( title, body, author ){
     return db.one(`INSERT INTO articles (title, body, author) VALUES ($1,$2,$3) RETURNING *`, [title, body, author])
       .catch(error =>{
@@ -22,52 +32,29 @@ const articleDataBase = () =>{
     });
   }
 
-//   function put( article ){
-//     let id = article.path;
-//     let searchedId = (id.split(''));
-//     searchedId.shift();
-//     let currentarticle = article.body;
-//     let articleById = checkForArticleById(searchedId.join(), currentarticle);
-//     if(articleById === false){
-//       return false;
-//     } else {
-//       articleById.title = article.body.title;
-//       articleById.body = article.body.body;
-//       articleById.author = article.body.author;
-//       return true;
-//     }
-//    }
-//   function deleteArticle(id){
-//     temp = id.split('');
-//     temp.shift();
-//     articleId = temp.join();
-//     let articleToDelete = checkForArticleById(articleId);
-//     articles.splice(articles.indexOf(articleToDelete),1);
-//     articleNames.splice(articleNames.indexOf(articleToDelete.name));
-//   }
+function put( title, body, author, id ){
+    newId = id.split('/').join('');
+    return db.none(`UPDATE articles SET (title, body, author) = ($1, $2, $3) WHERE id = $4`, [title, body, author, newId])
+      .catch(error =>{
+        console.log('ERROR: ', error);
+      });
+    }
 
-//   function checkForarticle( name ){
-//     if(articleNames.indexOf(name) < 0){
-//       return true;
-//     }
-//   }
-
-//   function checkForArticleById(id, currentarticle){
-//     if(articles.length === 0){
-//       return false;
-//     }
-//     for(var i = 0; i < articles.length; i++){
-//       if(articles[i].id == id){
-//         return articles[i];
-//       }
-//     } return false;
-//   }
+function deleteArticle(id){
+    newId = id.split('/').join('');
+    return db.none(`DELETE FROM articles WHERE id = $1`, [newId])
+      .catch(error=>{
+        console.log('ERROR: ', error);
+      });
+  }
+//
 
   return{
        get,
        post,
-//     put,
-//     deleteArticle,
+       getID,
+       put,
+       deleteArticle,
    };
  };
 
--- routes/articles.js
+++ routes/articles.js
@@ -10,8 +10,9 @@ router.route('/')
   .get(( req, res ) => {
     articleDataBase.get()
       .then((data) => {
+        console.log(data);
         res.render('article_index', {
-          article: data
+          articles: data
         });
       })
       .catch((error) => {
@@ -31,5 +32,77 @@ router.route('/')
           articles: data
         });
       });
+  })
+
+  .put(( req, res ) => {
+    res.send('PICK A THING TO PUT');
+  })
+
+  .delete(( req, res ) =>{
+    res.send('CANNOT DELETE NOTHING DUMBASS');
   });
+
+router.route('/new')
+  .get((req,res) =>{
+    res.render('article_new');
+  });
+
+router.route('/:id')
+  .get(( req, res ) =>{
+    console.log(req.path);
+    articleDataBase.getID(req.path)
+      .then(data => {
+        console.log('GET DATA' + data);
+        res.render('articles', {
+          articles: data
+        });
+      });
+  })
+
+.post(( req, res) =>{
+    res.send( 'CANNOT POST BY ID');
+  })
+
+.put(( req, res) =>{
+  let article_title = req.body.title;
+  let article_body = req.body.body;
+  let article_author = req.body.author;
+  let article_id = req.path;
+  articleDataBase.put(article_title, article_body, article_author, article_id)
+      .then(data => {
+        console.log('PUT DATA: ' + data);
+    });
+     articleDataBase.getID(req.path)
+      .then(data => {
+        res.render('articles', {
+        articles: data
+      });
+    });
+})
+
+.delete (( req, res ) =>{
+    articleDataBase.deleteArticle(req.path)
+      .then(data => {
+        console.log('DELETED?');
+        res.render('home');
+      });
+
+  });
+
+router.route('/:id/edit')
+  .get((req,res) =>{
+
+   let idpath = (req.path.split('/'));
+   idpath.pop();
+   newId = idpath.join('');
+
+  articleDataBase.getID(newId)
+    .then(data =>{
+      console.log(data);
+       res.render('article_edit', {
+         articles: data
+       });
+    });
+});
+
 
